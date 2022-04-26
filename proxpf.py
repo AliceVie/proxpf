42cc032c9864697f83c625dcb2e60a8174d6699a
Alice Vollink <me@alicevie.space>
2022-04-26 10:59:07 -0500

    Fix 0.0.0.0 ip address as wildcard

--- proxpf.py
+++ proxpf.py
@@ -13,9 +13,15 @@ def apply_rules(rules):
 		protocol = rule['type']
 
 		if (protocol == 'tcp' or protocol == 'both'):
-			os.system('iptables -t nat -A PREROUTING -p tcp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+			if (source_ip == '0.0.0.0'):
+				os.system('iptables -t nat -A PREROUTING -p tcp --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+			else:
+				os.system('iptables -t nat -A PREROUTING -p tcp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
 		if (protocol == 'udp' or protocol == 'both'):
-			os.system('iptables -t nat -A PREROUTING -p udp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+			if (source_ip == '0.0.0.0'):
+				os.system('iptables -t nat -A PREROUTING -p udp --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+			else:
+				os.system('iptables -t nat -A PREROUTING -p udp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
 
 def main():
 	rules = []
