7924e9a9a235c2a5593d5163787bd41c5ce32c4d
Alice Vollink <me@alicevie.space>
2022-04-26 10:40:59 -0500

    Initial commit, with example port definitions.

---
+++ proxpf.py
@@ -0,0 +1,32 @@
+import yaml
+import os
+
+def flush_iptables():
+	os.system('iptables -t nat -F PREROUTING')
+
+def apply_rules(rules):
+	for rule in rules:
+		source_ip = rule['source'].split(':')[0]
+		source_port = rule['source'].split(':')[1]
+		dest_ip = rule['dest'].split(':')[0]
+		dest_port = rule['dest'].split(':')[1]
+		protocol = rule['type']
+
+		if (protocol == 'tcp' or protocol == 'both'):
+			os.system('iptables -t nat -A PREROUTING -p tcp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+		if (protocol == 'udp' or protocol == 'both'):
+			os.system('iptables -t nat -A PREROUTING -p udp -s ' + source_ip + ' --dport' + source_port + ' -j REDIRECT --to ' + dest_ip + ':' + dest_port)
+
+def main():
+	rules = []
+
+	with open('./portdef.yml', 'r') as f:
+		rules = yaml.safe_load(f)['rules']
+
+	flush_iptables()
+	apply_rules(rules)
+
+if __name__ == "__main__":
+	main()
+
+
